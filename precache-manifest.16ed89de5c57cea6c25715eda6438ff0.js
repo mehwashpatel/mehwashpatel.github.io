41371642fae52f720efd75dd43444ca3185e4d7c
Mehwash Patel <mehwashpatel@Mehwashs-MacBook-Pro.local>
2020-04-12 17:29:19 -0400

    first build

---
+++ precache-manifest.16ed89de5c57cea6c25715eda6438ff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ec7c0bc30b13299a1f38246af1974d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04ab2b428748da216384",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c9eec62ce3b0c13cf2d",
+    "url": "/static/js/2.26b06e7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26b06e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ab2b428748da216384",
+    "url": "/static/js/main.be844e8e.chunk.js"
+  },
+  {
+    "revision": "6bf465cfb64fe7135efb",
+    "url": "/static/js/runtime-main.e0394d58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
